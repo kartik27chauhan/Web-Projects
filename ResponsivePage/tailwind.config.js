878ea588b1c5126c9e9f407e1a62763bd8f878d8
kartik27chauhan <kartikch863@gmail.com>
2024-11-13 13:18:24 +0530

    ResponsivePageusing HTMl & TailwindCSS

---
+++ ResponsivePage/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily: {
+        playfair:"'Playfair Display', serif",
+        lato : "'Lato', sans-serif",
+      }
+    },
+  },
+  plugins: [],
+}
